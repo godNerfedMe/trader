1a7b4cc073cc765b0cebe4bc7ef763647b540896
godNerfedMe <105129759+godNerfedMe@users.noreply.github.com>
2023-12-23 21:30:44 -0600

    Update analyze_commands.py

--- freqtrade/commands/analyze_commands.py
+++ freqtrade/commands/analyze_commands.py
@@ -1,14 +1,19 @@
+import os
 import logging
-from pathlib import Path
 from typing import Any, Dict
 
 from freqtrade.configuration import setup_utils_configuration
 from freqtrade.enums import RunMode
 from freqtrade.exceptions import OperationalException
 
-
+# Configure the logger
 logger = logging.getLogger(__name__)
 
+EXPORT_FILE_TYPES = {
+    RunMode.BACKTEST: 'signals',
+    RunMode.PAPERRUN: 'paper_results',
+    RunMode.LIVE: 'live_results',
+}
 
 def setup_analyze_configuration(args: Dict[str, Any], method: RunMode) -> Dict[str, Any]:
     """
@@ -19,30 +24,38 @@ def setup_analyze_configuration(args: Dict[str, Any], method: RunMode) -> Dict[s
     """
     config = setup_utils_configuration(args, method)
 
-    no_unlimited_runmodes = {
-        RunMode.BACKTEST: 'backtesting',
-    }
-    if method in no_unlimited_runmodes.keys():
-        from freqtrade.data.btanalysis import get_latest_backtest_filename
+    if method in EXPORT_FILE_TYPES:
+        export_type = EXPORT_FILE_TYPES[method]
+        export_filename = args.get('exportfilename')
 
-        if 'exportfilename' in config:
-            if config['exportfilename'].is_dir():
-                btfile = Path(get_latest_backtest_filename(config['exportfilename']))
-                signals_file = f"{config['exportfilename']}/{btfile.stem}_signals.pkl"
-            else:
-                if config['exportfilename'].exists():
-                    btfile = Path(config['exportfilename'])
-                    signals_file = f"{btfile.parent}/{btfile.stem}_signals.pkl"
-                else:
-                    raise OperationalException(f"{config['exportfilename']} does not exist.")
+        if export_filename:
+            if not os.path.exists(export_filename):
+                logger.error(f"The specified export file '{export_filename}' does not exist.")
+                raise OperationalException(
+                    f"The specified export file '{export_filename}' does not exist."
+                )
+            if not os.path.isfile(export_filename):
+                logger.error(f"The specified export file '{export_filename}' is not a valid file.")
+                raise OperationalException(
+                    f"The specified export file '{export_filename}' is not a valid file."
+                )
+
+            # Ensure the export file has the expected extension
+            expected_extension = f"{export_type}.pkl"
+            if not export_filename.endswith(expected_extension):
+                logger.error(
+                    f"The specified export file '{export_filename}' has an incorrect extension. "
+                    f"Expected: '{expected_extension}'."
+                )
+                raise OperationalException(
+                    f"The specified export file '{export_filename}' has an incorrect extension. "
+                    f"Expected: '{expected_extension}'."
+                )
+
+            config['exportfilename'] = export_filename
         else:
-            raise OperationalException('exportfilename not in config.')
-
-        if (not Path(signals_file).exists()):
-            raise OperationalException(
-                f"Cannot find latest backtest signals file: {signals_file}."
-                "Run backtesting with `--export signals`."
-            )
+            logger.error('--exportfilename argument not provided.')
+            raise OperationalException(f'--exportfilename argument not provided.')
 
     return config
 
@@ -55,9 +68,21 @@ def start_analysis_entries_exits(args: Dict[str, Any]) -> None:
     """
     from freqtrade.data.entryexitanalysis import process_entry_exit_reasons
 
-    # Initialize configuration
-    config = setup_analyze_configuration(args, RunMode.BACKTEST)
+    try:
+        # Initialize configuration
+        config = setup_analyze_configuration(args, RunMode.BACKTEST)
 
-    logger.info('Starting freqtrade in analysis mode')
+        logger.info('Starting freqtrade in analysis mode')
 
-    process_entry_exit_reasons(config)
+        process_entry_exit_reasons(config)
+
+        logger.info('Analysis completed successfully')
+
+    except OperationalException as ex:
+        logger.error(f'OperationalException during analysis: {ex}')
+    except FileNotFoundError as ex:
+        logger.error(f'FileNotFoundError during analysis: {ex}')
+    except Exception as ex:
+        logger.exception('An unexpected error occurred during analysis.')
+        raise ex
+
