5018d74ca4fa7aed8b3141d78fe689be7c311806
godNerfedMe <105129759+godNerfedMe@users.noreply.github.com>
2023-12-23 21:18:45 -0600

    Update test_interest.py

--- tests/leverage/test_interest.py
+++ tests/leverage/test_interest.py
@@ -5,38 +5,43 @@ from freqtrade.leverage import interest
 from freqtrade.util import FtPrecise
 
 
-ten_mins = FtPrecise(1 / 6)
-five_hours = FtPrecise(5.0)
-twentyfive_hours = FtPrecise(25.0)
+@pytest.fixture
+def borrowed_amount():
+    return FtPrecise(60.0)
 
 
-@pytest.mark.parametrize('exchange,interest_rate,hours,expected', [
-    ('binance', 0.0005, ten_mins, 0.00125),
-    ('binance', 0.00025, ten_mins, 0.000625),
-    ('binance', 0.00025, five_hours, 0.003125),
-    ('binance', 0.00025, twentyfive_hours, 0.015625),
-    # Kraken
-    ('kraken', 0.0005, ten_mins, 0.06),
-    ('kraken', 0.00025, ten_mins, 0.03),
-    ('kraken', 0.00025, five_hours, 0.045),
-    ('kraken', 0.00025, twentyfive_hours, 0.12),
-])
-def test_interest(exchange, interest_rate, hours, expected):
-    borrowed = FtPrecise(60.0)
+@pytest.fixture
+def exchange_params():
+    return {
+        'binance': {
+            'interest_rate': 0.00025,
+            'ten_mins_expected': 0.000625,
+            'five_hours_expected': 0.003125,
+            'twentyfive_hours_expected': 0.015625,
+        },
+        'kraken': {
+            'interest_rate': 0.00025,
+            'ten_mins_expected': 0.03,
+            'five_hours_expected': 0.045,
+            'twentyfive_hours_expected': 0.12,
+        },
+    }
 
-    assert pytest.approx(float(interest(
-        exchange_name=exchange,
-        borrowed=borrowed,
-        rate=FtPrecise(interest_rate),
-        hours=hours
-    ))) == expected
+
+def test_interest(exchange, borrowed_amount, exchange_params):
+    params = exchange_params[exchange]
+
+    def assert_interest(hours, expected):
+        result = interest(exchange_name=exchange, borrowed=borrowed_amount,
+                          rate=FtPrecise(params['interest_rate']), hours=hours)
+        assert pytest.approx(float(result)) == expected
+
+    assert_interest(1 / 6, params['ten_mins_expected'])
+    assert_interest(5.0, params['five_hours_expected'])
+    assert_interest(25.0, params['twentyfive_hours_expected'])
 
 
 def test_interest_exception():
     with pytest.raises(OperationalException, match=r"Leverage not available on .* with freqtrade"):
-        interest(
-            exchange_name='bitmex',
-            borrowed=FtPrecise(60.0),
-            rate=FtPrecise(0.0005),
-            hours=ten_mins
-        )
+        interest(exchange_name='bitmex', borrowed=FtPrecise(60.0),
+                 rate=FtPrecise(0.0005), hours=1 / 6)
